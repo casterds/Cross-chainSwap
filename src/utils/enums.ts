521a5cd6d757dc80166bf685eac6336384768313
casterds <96631866+casterds@users.noreply.github.com>
2023-05-12 23:21:06 +0530

    Create enums.ts

---
+++ src/utils/enums.ts
@@ -0,0 +1,7 @@
+// https://docs.axelar.dev/dev/reference/testnet-contract-addresses
+
+export enum SUPPORT_CHAIN_IDS {
+  MUMBAI = 80001, // Polygon testnet
+  BINANCE = 97, // BSC testnet
+  AVALANCHE = 43113, // Avalanche testnet
+}
