3b67ba96834afc023defd52775239074913f0769
casterds <96631866+casterds@users.noreply.github.com>
2023-05-12 22:50:20 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // To simplify, just move everything to the src folder.
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
